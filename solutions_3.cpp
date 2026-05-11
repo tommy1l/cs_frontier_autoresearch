75d03fad1cdc9aa65bdc40bba6b83fb6ba404449
CYB101NAME <CYB101EMAIL>
2026-05-11 10:56:50 -0700

    p3 score=66.667 chunk size 8.5M for fewer queries

--- solutions_3.cpp
+++ solutions_3.cpp
@@ -78,7 +78,7 @@ int main() {
             }
             ops.push_back(v);
         }
-        const size_t CHUNK = 7000000;
+        const size_t CHUNK = 8500000;
         size_t pos = 0;
         vector<int> resp;
         resp.reserve(ops.size());
