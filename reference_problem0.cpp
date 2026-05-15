5efaebacb21754add5d6648afff743cfa859e946
CYB101NAME <CYB101EMAIL>
2026-05-14 23:05:42 -0700

    p0 score=88.3444315742857 ord4 id ASC tiebreaker

--- reference_problem0.cpp
+++ reference_problem0.cpp
@@ -55,7 +55,7 @@ int main(){
             int db = min(ps[b].minW, ps[b].minH);
             if (da != db) return da < db;
             if (ps[a].k != ps[b].k) return ps[a].k < ps[b].k;
-            return ps[a].id > ps[b].id;
+            return ps[a].id < ps[b].id;
         });
         return res;
     };
