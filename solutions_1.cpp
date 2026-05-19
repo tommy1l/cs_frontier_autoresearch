cbd47b9db1cedaa0feed643d543cfc0c9fad3afb
CYB101NAME <CYB101EMAIL>
2026-05-19 16:13:32 -0700

    p1 score=99.23333333333333 3-way swap moves

--- solutions_1.cpp
+++ solutions_1.cpp
@@ -43,6 +43,119 @@ vector<ll> greedy_w(double w) {
     return greedy_fill_order(ord);
 }
 
+pair<ll, ll> best_ab(int i, int k, ll Sm, ll Sl, ll Amax, ll Bmax) {
+    ll best_val = 0;
+    ll best_a = 0, best_b = 0;
+    auto try_ab = [&](ll a, ll b) {
+        if (a < 0 || b < 0 || a > Amax || b > Bmax) return;
+        if (a*M[i] + b*M[k] > Sm) return;
+        if (a*L[i] + b*L[k] > Sl) return;
+        ll v = a*V[i] + b*V[k];
+        if (v > best_val) { best_val = v; best_a = a; best_b = b; }
+    };
+    try_ab(0, 0);
+    try_ab(Amax, 0);
+    try_ab(0, Bmax);
+    try_ab(Amax, Bmax);
+    {
+        ll rest_m = Sm - Amax*M[i];
+        ll rest_l = Sl - Amax*L[i];
+        if (rest_m >= 0 && rest_l >= 0) {
+            ll b = min({Bmax, rest_m/M[k], rest_l/L[k]});
+            try_ab(Amax, b);
+        }
+    }
+    {
+        ll rest_m = Sm - Bmax*M[k];
+        ll rest_l = Sl - Bmax*L[k];
+        if (rest_m >= 0 && rest_l >= 0) {
+            ll a = min({Amax, rest_m/M[i], rest_l/L[i]});
+            try_ab(a, Bmax);
+        }
+    }
+    {
+        ll b = min({Bmax, Sm/M[k], Sl/L[k]});
+        try_ab(0, b);
+    }
+    {
+        ll a = min({Amax, Sm/M[i], Sl/L[i]});
+        try_ab(a, 0);
+    }
+    long long det = (long long)M[i]*L[k] - (long long)M[k]*L[i];
+    if (det != 0) {
+        double a_d = (double)(Sm * L[k] - Sl * M[k]) / det;
+        double b_d = (double)(Sl * M[i] - Sm * L[i]) / det;
+        if (a_d >= -1 && b_d >= -1) {
+            for (ll da = -1; da <= 1; da++) {
+                for (ll db = -1; db <= 1; db++) {
+                    ll a = (ll)a_d + da;
+                    ll b = (ll)b_d + db;
+                    try_ab(a, b);
+                }
+            }
+        }
+    }
+    return {best_a, best_b};
+}
+
+bool triple_improve(vector<ll>& x, ll& val, ll& mass, ll& vol) {
+    bool any = false;
+    for (int j = 0; j < n; j++) {
+        if (x[j] == 0) continue;
+        ll xj = x[j];
+        for (int i = 0; i < n; i++) {
+            if (i == j) continue;
+            for (int k = i + 1; k < n; k++) {
+                if (k == j) continue;
+                ll Amax = Q[i] - x[i];
+                ll Bmax = Q[k] - x[k];
+                if (Amax < 0) Amax = 0;
+                if (Bmax < 0) Bmax = 0;
+                if (Amax == 0 && Bmax == 0) continue;
+
+                ll best_delta = 0;
+                ll best_rem = 0, best_a = 0, best_b = 0;
+                ll step = max((ll)1, xj / 200);
+                for (ll rem = 1; rem <= xj; rem += step) {
+                    ll Sm = M_CAP - mass + rem * M[j];
+                    ll Sl = L_CAP - vol + rem * L[j];
+                    auto [a, b] = best_ab(i, k, Sm, Sl, Amax, Bmax);
+                    ll delta = a*V[i] + b*V[k] - rem*V[j];
+                    if (delta > best_delta) {
+                        best_delta = delta;
+                        best_rem = rem;
+                        best_a = a;
+                        best_b = b;
+                    }
+                }
+                if (xj > 1) {
+                    ll Sm = M_CAP - mass + xj * M[j];
+                    ll Sl = L_CAP - vol + xj * L[j];
+                    auto [a, b] = best_ab(i, k, Sm, Sl, Amax, Bmax);
+                    ll delta = a*V[i] + b*V[k] - xj*V[j];
+                    if (delta > best_delta) {
+                        best_delta = delta;
+                        best_rem = xj;
+                        best_a = a;
+                        best_b = b;
+                    }
+                }
+
+                if (best_delta > 0) {
+                    x[j] -= best_rem;
+                    x[i] += best_a;
+                    x[k] += best_b;
+                    mass += -best_rem*M[j] + best_a*M[i] + best_b*M[k];
+                    vol += -best_rem*L[j] + best_a*L[i] + best_b*L[k];
+                    val += best_delta;
+                    any = true;
+                }
+            }
+        }
+    }
+    return any;
+}
+
 bool swap_improve(vector<ll>& x, ll& val, ll& mass, ll& vol) {
     bool any = false;
     for (int j = 0; j < n; j++) {
@@ -86,7 +199,10 @@ void local_search(vector<ll>& x, double time_limit_sec, chrono::steady_clock::ti
     ll val = eval_value(x, mass, vol);
     while (true) {
         if (chrono::duration<double>(chrono::steady_clock::now() - start).count() > time_limit_sec) break;
-        if (!swap_improve(x, val, mass, vol)) break;
+        bool a = swap_improve(x, val, mass, vol);
+        if (chrono::duration<double>(chrono::steady_clock::now() - start).count() > time_limit_sec) break;
+        bool b = triple_improve(x, val, mass, vol);
+        if (!a && !b) break;
     }
 }
 
