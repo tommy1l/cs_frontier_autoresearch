c34ed8a4c6c6054e6fc5bda80ee95509a173c35f
CYB101NAME <CYB101EMAIL>
2026-06-09 15:00:54 -0700

    p2 score=91.2900 paired binary search with disambiguation

--- split_solutions_2.cpp
+++ split_solutions_2.cpp
@@ -1,11 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+static int n;
+static int v_anchor;
+
+int doQuery(const vector<int>& q){
+    cout << 0;
+    for(int j = 1; j <= n; j++) cout << ' ' << q[j];
+    cout << '\n';
+    cout.flush();
+    int r;
+    cin >> r;
+    return r;
+}
+
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     
-    int n;
     cin >> n;
     
     if(n == 1){
@@ -15,7 +27,8 @@ int main(){
     }
     
     vector<int> hidden(n+1, 0);
-    int p_anchor = -1, v_anchor = -1;
+    int p_anchor = -1;
+    v_anchor = -1;
     
     for(int k = 1; k <= n; k++){
         int i;
@@ -48,27 +61,129 @@ int main(){
         if(i != p_anchor) G.push_back(i);
     }
     
+    vector<int> V;
     for(int v = 1; v <= n-1; v++){
-        if(v == v_anchor) continue;
+        if(v != v_anchor) V.push_back(v);
+    }
+    
+    size_t idx = 0;
+    while(idx + 1 < V.size()){
+        int a = V[idx], b = V[idx+1];
+        idx += 2;
         
+        vector<int> Ca = G;
+        vector<int> Cb = G;
+        
+        // Phase A: Ca == Cb
+        while(Ca == Cb && Ca.size() > 1){
+            int sz = Ca.size();
+            int m = (sz + 1) / 2;
+            vector<int> Sa(Ca.begin(), Ca.begin() + m);
+            vector<int> Sb(Ca.begin() + m, Ca.end());
+            
+            vector<int> q(n+1, v_anchor);
+            for(int j : Sa) q[j] = a;
+            for(int j : Sb) q[j] = b;
+            
+            int r = doQuery(q);
+            int sum = r - 1;
+            
+            if(sum == 0){
+                Ca = Sb;
+                Cb = Sa;
+            } else if(sum == 2){
+                Ca = Sa;
+                Cb = Sb;
+            } else {
+                // ambiguous
+                vector<int> q2(n+1, v_anchor);
+                for(int j : Sa) q2[j] = a;
+                int r2 = doQuery(q2);
+                int ba = r2 - 1;
+                if(ba == 1){
+                    Ca = Sa;
+                    Cb = Sa;
+                } else {
+                    Ca = Sb;
+                    Cb = Sb;
+                }
+            }
+        }
+        
+        // Phase B: disjoint
+        while(Ca.size() > 1 || Cb.size() > 1){
+            vector<int> Sa, Sb;
+            if(Ca.size() > 1){
+                int half = (Ca.size() + 1) / 2;
+                Sa.assign(Ca.begin(), Ca.begin() + half);
+            }
+            if(Cb.size() > 1){
+                int half = (Cb.size() + 1) / 2;
+                Sb.assign(Cb.begin(), Cb.begin() + half);
+            }
+            
+            vector<int> q(n+1, v_anchor);
+            for(int j : Sa) q[j] = a;
+            for(int j : Sb) q[j] = b;
+            
+            int r = doQuery(q);
+            int sum = r - 1;
+            
+            auto setMinus = [](const vector<int>& A, const vector<int>& B){
+                set<int> Bs(B.begin(), B.end());
+                vector<int> res;
+                for(int x : A) if(!Bs.count(x)) res.push_back(x);
+                return res;
+            };
+            
+            if(Sa.empty()){
+                // sum is indicator pos(b) in Sb
+                if(sum == 1) Cb = Sb;
+                else Cb = setMinus(Cb, Sb);
+            } else if(Sb.empty()){
+                if(sum == 1) Ca = Sa;
+                else Ca = setMinus(Ca, Sa);
+            } else {
+                if(sum == 0){
+                    Ca = setMinus(Ca, Sa);
+                    Cb = setMinus(Cb, Sb);
+                } else if(sum == 2){
+                    Ca = Sa;
+                    Cb = Sb;
+                } else {
+                    vector<int> q2(n+1, v_anchor);
+                    for(int j : Sa) q2[j] = a;
+                    int r2 = doQuery(q2);
+                    int ba = r2 - 1;
+                    int bb = 1 - ba;
+                    if(ba == 1) Ca = Sa;
+                    else Ca = setMinus(Ca, Sa);
+                    if(bb == 1) Cb = Sb;
+                    else Cb = setMinus(Cb, Sb);
+                }
+            }
+        }
+        
+        hidden[Ca[0]] = a;
+        hidden[Cb[0]] = b;
+        int pa = Ca[0], pb = Cb[0];
+        G.erase(remove_if(G.begin(), G.end(), [&](int x){ return x == pa || x == pb; }), G.end());
+    }
+    
+    // odd remaining
+    if(idx < V.size()){
+        int v = V[idx];
         vector<int> C = G;
-        
         while(C.size() > 1){
             int sz = C.size();
             int half = (sz + 1) / 2;
             vector<int> L(C.begin(), C.begin() + half);
-            unordered_set<int> Lset(L.begin(), L.end());
+            set<int> Lset(L.begin(), L.end());
             
             vector<int> q(n+1, v_anchor);
             for(int j : L) q[j] = v;
             
-            cout << 0;
-            for(int j = 1; j <= n; j++) cout << ' ' << q[j];
-            cout << '\n';
-            cout.flush();
-            
-            int a;
-            cin >> a;
+            int a = doQuery(q);
             int indicator = a - 1;
             
             if(indicator == 1){
