be44371f1c22196dd699ad27f2f5276f8e8dbe46
CYB101NAME <CYB101EMAIL>
2026-06-09 17:08:06 -0700

    p4 score=0.0000 cached-value-guided binary search with countLE ear

--- split_solutions_4.cpp
+++ split_solutions_4.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int n;
 long long k;
 unordered_map<long long, long long> cache;
+set<long long> cachedVals;
 
 long long query(int i, int j) {
     long long key = (long long)i * 200001LL + j;
@@ -13,7 +14,8 @@ long long query(int i, int j) {
     cout.flush();
     long long v;
     cin >> v;
-    cache[key] = v;
+    auto res = cache.insert({key, v});
+    if (res.second) cachedVals.insert(v);
     return v;
 }
 
@@ -21,9 +23,12 @@ long long countLE(long long v) {
     long long cnt = 0;
     int i = 1, j = n;
     while (i <= n && j >= 1) {
+        long long maxFuture = (long long)(n - i + 1) * j;
+        if (cnt + maxFuture < k) return cnt;
         long long x = query(i, j);
         if (x <= v) {
             cnt += j;
+            if (cnt >= k) return cnt;
             i++;
         } else {
             j--;
@@ -40,48 +45,75 @@ int main() {
         cout.flush();
         return 0;
     }
-    long long a11 = query(1, 1);
-    long long ann = query(n, n);
+    long long lo = query(1, 1);
+    long long hi = query(n, n);
     if (k == 1) {
-        cout << "DONE " << a11 << "\n";
+        cout << "DONE " << lo << "\n";
         cout.flush();
         return 0;
     }
     if (k == (long long)n * n) {
-        cout << "DONE " << ann << "\n";
+        cout << "DONE " << hi << "\n";
         cout.flush();
         return 0;
     }
     
     vector<long long> diag(n + 1, 0);
-    diag[1] = a11;
-    diag[n] = ann;
+    diag[1] = lo;
+    diag[n] = hi;
     for (int i = 2; i <= n - 1; i++) {
         diag[i] = query(i, i);
     }
     
-    int lo_i = 1, hi_i = n;
-    while (lo_i < hi_i) {
-        int mid_i = (lo_i + hi_i) / 2;
-        if (countLE(diag[mid_i]) >= k) hi_i = mid_i;
-        else lo_i = mid_i + 1;
+    long long target_hi = k;
+    int i_hi = -1;
+    for (int i = 1; i <= n; i++) {
+        if ((long long)i * i >= target_hi) {
+            i_hi = i;
+            break;
+        }
+    }
+    if (i_hi != -1) {
+        hi = diag[i_hi];
     }
     
-    long long vlo = (lo_i > 1) ? diag[lo_i - 1] + 1 : a11;
-    long long vhi = diag[lo_i];
+    long long target_lo = (long long)n * n - k + 1;
+    int i_lo = -1;
+    for (int i = n; i >= 1; i--) {
+        long long m = n - i + 1;
+        if (m * m >= target_lo) {
+            i_lo = i;
+            break;
+        }
+    }
+    if (i_lo != -1) {
+        lo = diag[i_lo];
+    }
     
-    if (vlo >= vhi) {
-        cout << "DONE " << vhi << "\n";
+    if (lo == hi) {
+        cout << "DONE " << lo << "\n";
         cout.flush();
         return 0;
     }
     
-    while (vlo < vhi) {
-        long long mid = vlo + (vhi - vlo) / 2;
-        if (countLE(mid) >= k) vhi = mid;
-        else vlo = mid + 1;
+    while (lo < hi) {
+        long long vlo = lo, vhi = hi;
+        long long mid;
+        auto itLo = cachedVals.upper_bound(vlo);
+        auto itHi = cachedVals.upper_bound(vhi);
+        long long rangeSize = 0;
+        for (auto it = itLo; it != itHi; ++it) rangeSize++;
+        if (rangeSize >= 1) {
+            auto it = itLo;
+            advance(it, rangeSize / 2);
+            mid = *it;
+        } else {
+            mid = vlo + (vhi - vlo) / 2;
+        }
+        if (countLE(mid) >= k) hi = mid;
+        else lo = mid + 1;
     }
-    cout << "DONE " << vlo << "\n";
+    cout << "DONE " << lo << "\n";
     cout.flush();
     return 0;
 }
